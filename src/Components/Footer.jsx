95ee0806b023f2bc44ff90481fbc61abb49fa686
khushbupatel05 <khushbupatel0009@gmail.com>
2025-05-22 11:44:56 +0530

    commit for edit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='footer-img'>
-      <div className='ps-5'>
+      <div className='ps-5 '>
         <div className="row d-flex align-items-center justify-content-between">
           <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-            <div className='my-5 '>
+            <div className='my-5 ps-5 '>
               <img src="/Img/logo.png" alt="" />
             </div>
           </div>
           <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-            <div className='my-5  '>
+            <div className='my-5 ps-5 '>
               <ul className='d-flex footer-text gap-3 fs-5 circle-icon rounded-circle'>
                 <li className=''><i class="ri-facebook-fill"></i></li>
                 <li className='icon'><i class="ri-linkedin-fill"></i></li>
@@ -23,7 +23,7 @@ const Footer = () => {
           <div class="col-lg-4 col-md-12 col-sm-12 col-12">
             <div class="   ">
               <div class="text d-flex footer-text gap-3 fs-3 my-5 pt-2  " >
-                <p className='fs-6 ' >Copyright © 2022 Rainbow-Themes. All Rights Reserved.</p>
+                <p className='fs-6  ps-5' >Copyright © 2022 Rainbow-Themes. All Rights Reserved.</p>
               </div>
             </div>
           </div>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,39 +1,42 @@
-import React from 'react'
+import React from 'react';
 
 const Header = () => {
   return (
     <header className=''>
-       <div className="container">
-          <div className=' d-flex align-items-center justify-content-between top-header'>
-              <div className='d-flex align-items-center my-3'>
-                  <div className="logo ">
-                    <img src="/Img/logo-symbol-dark.png" alt="" />
-                  </div>
-                  <nav className=''>
-                    <ul className='d-flex align-items-center justify-content-center gap-3 fw-semibold fs-6 pt-2'>
-                      <li className='nav'>Home</li>
-                      <li className='nav'>About</li>
-                      <li className='nav'>Service</li>
-                      <li className='nav'>Portfolio</li>
-                      <li className='nav'>Testimonial</li>
-                      <li className='nav'>Blog</li>
-                      <li className='nav'>Contact</li>
-                    </ul>
-                  </nav>
-              </div>
-              <div className='d-flex gap-3'>
-                <ul className='text-dark d-flex gap-3 my-3'>
-                  <li className='icon' ><i class="bi bi-facebook"></i></li>
-                  <li className='icon'><i class="bi bi-linkedin"></i></li>
-                  <li className='icon'><i class="bi bi-instagram"></i></li>
-                  <li className='icon'><i class="bi bi-twitter"></i></li>
-                </ul>
-                <button className=' rounded-2 justify-content-center align-items-center d-flex '>BUY NOW</button>
-              </div> 
+      <div className="container">
+        <div className='d-flex align-items-center justify-content-between top-header'>
+          <div className='d-flex align-items-center my-3'>
+            <div className="logo">
+              <img src="/Img/logo-symbol-dark.png" alt="Logo" />
+            </div>
+            <nav className='ms-5'>
+              <ul className='menu d-flex align-items-center justify-content-center gap-3 fw-semibold fs-6 pt-2'>
+                <li className='nav'><a href="#">Home</a></li>
+                <li className='nav'><a href="#">About</a></li>
+                <li className='nav'><a href="#">Service</a></li>
+                <li className='nav'><a href="#">Portfolio</a></li>
+                <li className='nav'><a href="#">Testimonial</a></li>
+                <li className='nav'><a href="#">Blog</a></li>
+                <li className='nav'><a href="#">Contact</a></li>
+              </ul>
+            </nav>
           </div>
-       </div>
+          <div className='d-flex gap-3'>
+            <ul className='text-dark d-flex gap-3 my-3'>
+              <li className='icon'><i className="bi bi-facebook"></i></li>
+              <li className='icon'><i className="bi bi-linkedin"></i></li>
+              <li className='icon'><i className="bi bi-instagram"></i></li>
+              <li className='icon'><i className="bi bi-twitter"></i></li>
+            </ul>
+            <button className='rounded-2 justify-content-center align-items-center d-flex btn-custom'>BUY NOW</button>
+            <button className="menubar d-lg-none d-flex p-2">
+              <i className="bi bi-list"></i>
+            </button>
+          </div>
+        </div>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
